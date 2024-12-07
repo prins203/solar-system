ff53f908dfb6f278c54dcc1b9d7fc3c690eafdcb
prins203 <50384131+prins203@users.noreply.github.com>
2024-12-07 17:17:03 -0500

    loading .env

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors')
+require('dotenv').config();
 
 
 app.use(bodyParser.json());
